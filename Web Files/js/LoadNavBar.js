964784504d30a5a5ae83c2ca188ea82a3b46f0b9
Michael Yslas <Michael Yslas>
2016-05-23 15:04:57 -0700

    visual changes

--- Web Files/js/LoadNavBar.js
+++ Web Files/js/LoadNavBar.js
@@ -10,7 +10,7 @@ var navBarText = "<header>\n\
       </a>\n\
 \t\t<ul>\n\
 \t\t\t<li><a href='index.html'><p>Home</p></a></li>\n\
-\t\t\t<li><a href='Explore.html'><p>Explore Topics <span class='glyphicon glyphicon-chevron-down'></span></p></a>\n\
+\t\t\t<li><a href='Explore.html'><p>Explore our Visualizations <span class='glyphicon glyphicon-chevron-down'></span></p></a>\n\
 \t\t\t\t<ul>\n\
 \t\t\t\t\t<li><p>About the School <span class='glyphicon glyphicon-chevron-right'></span></p>\n\
 \t\t\t\t\t\t<ul>\n\
@@ -40,8 +40,8 @@ var navBarText = "<header>\n\
 \t\t\t\t\t</li>\n\
 \t\t\t\t</ul>\n\
 \t\t\t</li>\n\
-\t\t\t<li><a href='Visualizations/MYO.html'><p>Make your Own</p></a></li>\n\
-\t\t\t<li><a href='DownloadData.html'><p>Download Data</p></a></li>\n\
+\t\t\t<li><a href='Visualizations/MYO.html'><p>Compare Your Own Visualizations</p></a></li>\n\
+\t\t\t<li><a href='DownloadData.html'><p>Download The Data</p></a></li>\n\
 \t\t</ul>\n\
 \t</nav>\n\
 </header>";
